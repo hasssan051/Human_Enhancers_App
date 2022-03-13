1a4e495b76ae262a7420ea29a7cc385096a2c92c
iamzaidi <hassanzaidi4@gmail.com>
2022-03-13 05:22:40 +0500

    Created forms for registration and login
    This includes adding the validation to these forms in the html file.

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,flash, redirect
 from form import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -6,28 +6,37 @@ app = Flask(__name__)
 # remember to turn this into an environment variable later on
 app.config['SECRET_KEY'] = '88d99ae8d44e1eb62cd8d4f7c6dc1034'
 
-@app.route("/home")
-def home():
-    return render_template('home.html', title = 'Home')
-
 @app.route("/")
 @app.route("/search")
 def search():
-    return render_template('search.html')
-
+    return render_template('search.html', title='Query the Database')
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+@app.route("/search_table")
+def search_table():
+    return render_template('search_table.html', title='Search Further')
+
+@app.route("/help")
+def help():
+    return render_template('help.html', title='Help')
+
+@app.route("/register",methods=['GET','POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html', title='Register', form=form)
+    if form.validate_on_submit():
+        flash(f' You can now start using ENHANCER DB {form.firstname.data}!','success')
+        return redirect(url_for('search'))
+    return render_template('register.html', title='Join Today', form=form)
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f' You have been logged in!','success')
+        return redirect(url_for('search'))
     return render_template('login.html', title='Login', form=form)
 
 
--- form.py
+++ form.py
@@ -1,22 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField, SubmitField, BooleanField
+from wtforms import StringField,PasswordField, SubmitField, BooleanField,SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired(),Length(min=2,max=26)])
     lastname = StringField('Last Name', validators=[DataRequired(),Length(min=2,max=26)])
-    username = StringField('Username', validators=[DataRequired(),Length(min=2,max=20)])
     email = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=8),EqualTo('password')])
-    affiliations = StringField('Affiliations', validators=[Length(min=2,max=150)])
-
-
-    submit = StringField('Sign Up')
+    occupation = SelectField('Occupation',choices=['Student','Researcher','Random Dude','With an Organization'])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     remember = BooleanField('Remember Me')
 
-    submit = StringField('Log in')
+    submit = SubmitField('Log in')
